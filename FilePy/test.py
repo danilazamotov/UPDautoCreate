4692b792efe8316a04e2174eb1e7b7a73fe51e39
Danila <ddanilazamotov@gmail.com>
2022-10-03 23:34:55 +0300

    v0.4 + add func rename xml in folders + ModifyXMLFile add some feature + etc

--- FilePy/test.py
+++ FilePy/test.py
@@ -43,13 +43,7 @@ def SendAttribute(path_xml):
 def modifyIDRCC(ID, RCC):
     # global
     num_out = 1412
-    return str(ID[14:-41]) + "-" + str(num_out), str(RCC[24:-38]) + "-" + str(num_out)
-
-
-def modify_name_xml(path_xml):
-    # global
-    num_out = 1412
-    return str(path_xml.split("//")[-1].split(".")[0]) + str(num_out)
+    return str(ID[14:-41]) + "-" + str(num_out), str(RCC[24:-37]) + "-" + str(num_out)
 
 
 def RedirectFile(path_xml, path_to_main_folder_checks):
@@ -58,46 +52,47 @@ def RedirectFile(path_xml, path_to_main_folder_checks):
         os.chdir(i)
         try:
             sh.copy(path_xml, os.getcwd())
-            print("Copy ok")
         except OSError:
             print("Error in copy")
         os.chdir(path_to_main_folder_checks)
+    print("Copy xml ok")
 
 
-def ModifyXMLFile(path_to_main_folder_checks, Id_name, Id_name_modify):
+def ModifyXMLFile(path_to_main_folder_checks, Id_name, Id_name_modify, Num_chf, rcc_name):
     os.chdir(path_to_main_folder_checks)
     for i in os.listdir(os.getcwd()):
         os.chdir(i)
         for j in os.listdir(os.getcwd()):
             path_local_check_xml_file = os.getcwd() + '//' + j
-            print(j)
             try:
                 with open(path_local_check_xml_file, 'r', encoding='Windows-1251') as f:
                     xml_data = f.read()
                     xml_data = re.sub(Id_name[14:-41], Id_name_modify, xml_data)
+                    xml_data = re.sub(Num_chf[23:-35], '"' + rcc_name + '" ', xml_data)
                 f.close()
                 with open(j, 'w', encoding='Windows-1251') as f:
                     f.write(xml_data)
                 f.close()
             except IOError:
-                print("Error in modify_in_xml")
-
+                print("ModifyInXMLFile")
         os.chdir(path_to_main_folder_checks)
+    print("Change xml inside successfully")
+        #изменить имя каждого из файлов в цикле j и rcc
 
 
-def ModifyAttributeXMLFile(path_xml, modify_):
-    try:
-        with open(path, 'r', encoding='Windows-1251') as f:
-            xml_file = f.read()
-            xml_file = re.sub(Id_file[14:-41], x, xml_file)
-            print("Modify file successful")
-        f.close()
-    except IOError:
-        print("Error in ")
-    with open(path, 'w', encoding='Windows-1251') as f:
-        f.write(xml_file)
-    f.close()
-
+def ModifyAttributeXMLFile(path_to_main_folder_checks):
+    num_out = 1410002
+    os.chdir(path_to_main_folder_checks)
+    for i in os.listdir(os.getcwd()):
+        os.chdir(i)
+        x1 = path_to_main_folder_checks + "//" + i
+        for root, dirs, files in os.walk(x1):
+            for file in files:
+                xx = AutoParsePathXLSX(str(path_to_main_folder_checks + "////" + i + "////" + ''.join(file)))
+                yy = str(xx).split("////")[-1].split(".")[0] + str(num_out) + ".xml"
+                zz = AutoParsePathXLSX(x1 + "////" + yy)
+                os.rename(xx, zz)
+        os.chdir(path_to_main_folder_checks)
 
 # C:\Users\zamot\OneDrive\Документы\GitHub\UPDautoCreate\XMLandXML\ON_NSCHFDOPPRPROS_2BM-2310031475-2012070307370849459200000000000_2BM-9718070427-771801001-20220921-.xml
 if __name__ == '__main__':
@@ -107,30 +102,11 @@ if __name__ == '__main__':
     # исходники
     Id_file, Num_chf = SendAttribute(path)
 
-    # модифиц имя
-    name_xml = modify_name_xml(path)
-
-    # for i in os.listdir(getPathChecks()):
-    # sh.copy(path, r'C://Users//zamot//OneDrive//Документы\GitHub\UPDautoCreate\Checks\CHCK_01_00')
-
     # модиф атрибуты
     id_name, rcc_name = modifyIDRCC(Id_file, Num_chf)[0], modifyIDRCC(Id_file, Num_chf)[1]
 
-    # print(x,'\f ', y)
-
-    # with open(path, 'r', encoding='Windows-1251') as files:
-    #     xml_data = files.read()
-    #     xml_data = re.sub(Id_file[14:-41], x, xml_data)
-    #     print(xml_data)
-    # files.close()
-    #
-    # with open(path, 'w', encoding='Windows-1251') as files:
-    #     files.write(xml_data)
-    # files.close()
-
-    # asdasd =
     getPathChecks = getPathChecks()
-    #print(path)
+    # реализовать для RedirectFile - если в папке есть что-то - не создавать
     RedirectFile(path, getPathChecks)
-
-    ModifyXMLFile(getPathChecks, Id_file, id_name)
+    ModifyXMLFile(getPathChecks, Id_file, id_name, Num_chf, rcc_name)
+    ModifyAttributeXMLFile(getPathChecks)
